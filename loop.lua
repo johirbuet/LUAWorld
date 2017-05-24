9af177cb1b40222d861f0e643a11dbd1aec50bbb
johirbuet <johirbuet@gmail.com>
2017-05-23 23:06:06 -0500

    Three more files

---
+++ loop.lua
@@ -0,0 +1,7 @@
+a = {}
+for i=1,10 do
+	a[i] = io.read()
+	end
+for i,line in ipairs(a) do
+	print(line)
+	end
---
+++ number.lua
@@ -0,0 +1,7 @@
+line = io.read()
+n = tonumber(line)
+if n == nil then
+	print("Not a valid number")
+else
+	print(n*2)
+	end
